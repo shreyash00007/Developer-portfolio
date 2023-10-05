b29c907f720d1dcfdd93901fb8cbceeb1952a987
shreyash dhanawade <shreyashdhanawadecode@gmail.com>
2023-10-05 22:36:24 +0530

    update

---
+++ src/components/footer.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function Footer() {
+    return (
+        <div>
+            <h1 className="text-3xl ">
+                Hello world!
+            </h1>
+        </div>
+    );
+}
+
+export default Footer;
